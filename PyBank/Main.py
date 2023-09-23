b38aca77fff7f81940d769936c44be62ed1bfe93
JeneenGnilka <142128170+JeneenGnilka@users.noreply.github.com>
2023-09-23 18:39:32 -0400

    Updates to homework

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -6,18 +6,44 @@ import csv
 # Import csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# variable to count date
+# Lists to store data
+date = []
+profit_loss = []
+monthly_change = []
+
+prv_month_chg = 0
 
 # Read CSV
+with open(csvpath, encoding='utf-8') as csvfile:
 
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # skip header
+    csv_header = next(csvfile)
+
+    for row in csv.reader(csvfile):
+    
+
+        # Add Date
+        date.append(row[0])
+
+        # Add Profit/Loss
+        profit_loss.append([1])
+
+        # Calculate change from previous month and add monthly_change
+        prv_month_chg = (int(row[1]) - ((row-1)[1]))
+        monthly_change.append(int(prv_month_chg))
+
+   # print(date, profit_loss)
+
+# Read CSV
 with open(csvpath, encoding='utf') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first 
+    #Read the header row first 
     csv_header = next(csvfile)
-
     
     # Calculate total profit/loss
     total = 0
@@ -30,6 +56,16 @@ with open(csvpath, "r") as f:
     reader = csv.reader(f,delimiter =",")
     data = list(reader)
     row_count = len(data)
+
+
+with open(csvpath, "r") as f:
+    reader = csv.reader(f,delimiter =",") 
+  
+  
+  #  Change_Prev_Mon = []
+   
+   # for i in csv.reader(csvfile):
+    #    Change_Prev_Mon.append([0,] + [int[i-1]])
 
 print (f"Monthes: {row_count-1}")
 print(f"Total: ${total}")
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,1 +1,45 @@
-  
+# Modules
+
+import os
+import csv
+
+# Import csv
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+    # Read CSV
+with open(csvpath, encoding='utf') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvfile)
+    print(csvreader)
+    print(csv_header)
+
+# count total votes in data
+with open(csvpath, "r") as f:
+    reader = csv.reader(f,delimiter =",")
+    data = list(reader)
+    row_count = len(data)
+
+print(f"Total Votes: {row_count-1}")
+
+# variables for vote count
+charles_vote = 0
+diana_vote = 0
+raymon_vote = 0
+
+# Calculate vote totals
+with open(csvpath, "r") as g:
+    reader = csv.reader(g,delimiter =",")
+    votes = list(reader)
+    for votes in reader:
+        if votes == "Charles Casper Stockham":
+            charles_vote += 1
+        elif votes == "Diana DeGette":
+            diana_vote += 1
+        elif votes == "Raymon Anthony Doane":
+            raymon_vote += 1
+
+print(charles_vote, " ", diana_vote, " ", raymon_vote)
+
