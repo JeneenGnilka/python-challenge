67f2efe875984e3257e0bd0d1cfa6c11ad91e158
JeneenGnilka <142128170+JeneenGnilka@users.noreply.github.com>
2023-09-24 22:14:23 -0400

    Final version of homework

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -10,17 +10,20 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 date = []
 profit_loss = []
 monthly_change = []
-
+# variables
+total_change = 0
 prv_month_chg = 0
-
+max_num = 0
 # Read CSV
-with open(csvpath, encoding='utf-8') as csvfile:
+with open(csvpath, encoding='utf') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     # skip header
     csv_header = next(csvfile)
+    print(csv_header)
 
+    # Calculate column of net change of current month compared to prior month
     for row in csv.reader(csvfile):
     
 
@@ -28,50 +31,42 @@ with open(csvpath, encoding='utf-8') as csvfile:
         date.append(row[0])
 
         # Add Profit/Loss
-        profit_loss.append([1])
+        profit_loss.append(int(row[1]))
 
-        # Calculate change from previous month and add monthly_change
-        prv_month_chg = (int(row[1]) - ((row-1)[1]))
-        monthly_change.append(int(prv_month_chg))
-
-   # print(date, profit_loss)
-
-# Read CSV
-with open(csvpath, encoding='utf') as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    #Read the header row first 
-    csv_header = next(csvfile)
-    
-    # Calculate total profit/loss
-    total = 0
-    for row in csv.reader(csvfile):
-        total += int(row[1])
+    # calculate change in Profit/Loss from prior month
+    for x, y in zip(profit_loss[0::], profit_loss[1::]):
+        monthly_change.append(y-x)
 
 
-# count months in data
-with open(csvpath, "r") as f:
-    reader = csv.reader(f,delimiter =",")
-    data = list(reader)
-    row_count = len(data)
+# Total profit
+total_P_L = sum(profit_loss)
 
+# Total Months
+total_months = (len(date))
 
-with open(csvpath, "r") as f:
-    reader = csv.reader(f,delimiter =",") 
-  
-  
-  #  Change_Prev_Mon = []
-   
-   # for i in csv.reader(csvfile):
-    #    Change_Prev_Mon.append([0,] + [int[i-1]])
-
-print (f"Monthes: {row_count-1}")
-print(f"Total: ${total}")
+# Average Change
+import statistics
+avg_chg = round(statistics.mean(monthly_change),2)
     
 
+# Print total months
+print (f"Months: {total_months}")
+# Print total of Profit/Loss
+print(f"Total: ${total_P_L}")
+# Print Average Change 
+print(f"Average Change: {avg_chg}")
+# Greatest increase in Profits
+print(f"Greatest Increase in Profits: {(date[monthly_change.index(max(monthly_change))+1])} ${(max(monthly_change))}")
+# Greatest decrease in profits
+print(f"Greatest Increase in Profits: {(date[monthly_change.index(min(monthly_change))+1])} ${(min(monthly_change))}")
 
-    
-
-
+# Print Financial Analysis to text file
+f = open("Financial_Analysis.txt", "w")
+print("Financial Analysis", file=f)
+print("________________________________", file=f)
+print (f"Months: {total_months}", file=f)
+print(f"Total: ${total_P_L}", file=f)
+print(f"Average Change: ${avg_chg}", file=f)
+print(f"Greatest Increase in Profits: {(date[monthly_change.index(max(monthly_change))+1])} (${(max(monthly_change))})", file=f)
+print(f"Greatest Increase in Profits: {(date[monthly_change.index(min(monthly_change))+1])} (${(min(monthly_change))})", file=f)
+f.close()
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -6,14 +6,14 @@ import csv
 # Import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-    # Read CSV
+# Read CSV
 with open(csvpath, encoding='utf') as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
+# CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
+# to skip header
     csv_header = next(csvfile)
-    print(csvreader)
+
     print(csv_header)
 
 # count total votes in data
@@ -21,25 +21,52 @@ with open(csvpath, "r") as f:
     reader = csv.reader(f,delimiter =",")
     data = list(reader)
     row_count = len(data)
+    total_votes = row_count-1
 
-print(f"Total Votes: {row_count-1}")
+print(f"Total Votes: {total_votes}")
 
 # variables for vote count
 charles_vote = 0
 diana_vote = 0
 raymon_vote = 0
 
-# Calculate vote totals
-with open(csvpath, "r") as g:
+# Calculate vote totals for the candidates
+with open(csvpath) as g:
     reader = csv.reader(g,delimiter =",")
-    votes = list(reader)
-    for votes in reader:
-        if votes == "Charles Casper Stockham":
+
+    for rows in reader:
+        rows
+
+        if rows[2] == "Charles Casper Stockham":
             charles_vote += 1
-        elif votes == "Diana DeGette":
+        elif rows[2] == "Diana DeGette":
             diana_vote += 1
-        elif votes == "Raymon Anthony Doane":
+        elif rows[2] == "Raymon Anthony Doane":
             raymon_vote += 1
 
-print(charles_vote, " ", diana_vote, " ", raymon_vote)
+# To calculate percentage won by each candidate
+charles_percent = round(charles_vote / total_votes * 100, 3)
+diana_percent = round(diana_vote / total_votes * 100,3)
+raymon_percent = round(raymon_vote / total_votes * 100,3)
 
+
+# Print election results
+print(f"Charles Casper Stockeham: {charles_percent}% ({charles_vote})") 
+print(f"Diana DeGette: {diana_percent}% ({diana_vote})")
+print(f"Raymon Anthony Doane: {raymon_percent}% ({raymon_vote})")
+
+print("Winner: Diana DeGette")
+
+# Print election results to text file
+f = open("election_results.txt", "w")
+print("Election_Results", file=f)
+print("________________________________", file=f)
+print(f"Total Votes: {total_votes}", file=f)
+print("________________________________", file=f)
+print(f"Charles Casper Stockeham: {charles_percent}% ({charles_vote})", file=f) 
+print(f"Diana DeGette: {diana_percent}% ({diana_vote})", file=f)
+print(f"Raymon Anthony Doane: {raymon_percent}% ({raymon_vote})", file=f)
+print("________________________________", file=f)
+print("Winner: Diana DeGette", file=f)
+print("________________________________", file=f)
+f.close()
